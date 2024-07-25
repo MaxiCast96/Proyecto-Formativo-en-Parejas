06b924cbf980da7c1c2b58795c450f742a1b1b20
Guillermo Chavez <20230075@ricaldone.edu.sv>
2024-07-24 22:32:45 -0600

    Conexion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation("com.oracle.database.jdbc:ojdbc6:11.2.0.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
---
+++ app/src/main/java/Modelo/ClassConexion.kt
@@ -0,0 +1,20 @@
+package modelo
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ClassConexion {
+    fun cadenaConexion(): Connection? {
+        try {
+            val ip = "jdbc:oracle:thin:@192.168.1.17:1521:xe"
+            val usuario = "Guille_Developer"
+            val contrasena = "holacanola123"
+
+            val conexion = DriverManager.getConnection(ip, usuario, contrasena)
+            return conexion
+        } catch (e: Exception) {
+            println("El error es este: $e")
+            return null
+        }
+    }
+}
